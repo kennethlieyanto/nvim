9d6efd461879c6af3a238205329b70b9976adb8b
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2026-07-27 11:56:44 +0700

    auto reload files when changed outside

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,13 @@
+vim.opt.autoread = true
+
+vim.api.nvim_create_autocmd({ "FocusGained", "VimResume", "TermClose", "TermEnter" }, {
+	desc = "Check for external file changes",
+	group = vim.api.nvim_create_augroup("checktime-on-focus", { clear = true }),
+	callback = function()
+		vim.cmd("checktime")
+	end,
+})
+
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
