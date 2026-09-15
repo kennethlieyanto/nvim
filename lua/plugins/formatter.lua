8233224f7e8237417295322037b54e9e6c3ff59f
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2026-09-15 16:49:00 +0700

    WIP

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,42 +1,43 @@
 return {
-	"stevearc/conform.nvim",
-	dependencies = { "mason.nvim" },
-	event = { "BufWritePre" },
-	keys = {
-		{
-			"<leader>cf",
-			function()
-				require("conform").format({ async = true })
-			end,
-			mode = "",
-			desc = "Format buffer",
-		},
-	},
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				javascript = { "biome" },
-				typescript = { "biome" },
-				javascriptreact = { "biome" },
-				typescriptreact = { "biome" },
-				css = { "biome" },
-				html = { "biome" },
-				json = { "biome" },
-				yaml = { "prettier" },
-				cs = { "csharpier" },
-				nix = { "alejandra" },
-			},
-			format_on_save = {
-				timeout_ms = 3000,
-				lsp_format = "fallback",
-			},
-			formatters = {
-				csharpier = {
-					command = "csharpier",
-					args = { "format", "--write-stdout" },
-				},
-			},
-		})
-	end,
+    "stevearc/conform.nvim",
+    dependencies = { "mason.nvim" },
+    event = { "BufWritePre" },
+    keys = {
+        {
+            "<leader>cf",
+            function()
+                require("conform").format({ async = true })
+            end,
+            mode = "",
+            desc = "Format buffer",
+        },
+    },
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                javascript = { "biome" },
+                typescript = { "biome" },
+                javascriptreact = { "biome" },
+                typescriptreact = { "biome" },
+                css = { "biome" },
+                html = { "biome" },
+                json = { "biome" },
+                yaml = { "prettier" },
+                cs = { "csharpier" },
+                nix = { "alejandra" },
+                markdown = { "prettierd" }
+            },
+            format_on_save = {
+                timeout_ms = 3000,
+                lsp_format = "fallback",
+            },
+            formatters = {
+                csharpier = {
+                    command = "csharpier",
+                    args = { "format", "--write-stdout" },
+                },
+            },
+        })
+    end,
 }
