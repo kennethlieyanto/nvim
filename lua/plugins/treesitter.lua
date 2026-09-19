a8338bc8d75205dc5f3291a22e5190e167da5a45
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2026-09-19 16:55:18 +0700

    fix treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,22 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
     lazy = false,
+    build = ":TSUpdate",
     init = function()
+        local ts = require("nvim-treesitter")
+        local ensure_installed = { "bash", "c_sharp", "lua" }
+        ts.install(ensure_installed)
+    end,
+    config = function()
         vim.api.nvim_create_autocmd('FileType', {
+            pattern = { '*' },
             callback = function()
-                -- Enable treesitter highlighting and disable regex syntax
+                -- Enable treesitter highlighting
                 pcall(vim.treesitter.start)
-                -- Enable treesitter-based indentation
+
+                -- Enable treesitter based indentation
                 vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
             end,
         })
-        -- ...
     end,
 }
