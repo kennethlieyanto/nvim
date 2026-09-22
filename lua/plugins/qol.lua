7b7c5ced776ba2248d0ef1f8ec3ba688b43cbd6c
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2026-09-22 16:55:55 +0700

    add moving animated cursor and smoothscrolling

--- lua/plugins/qol.lua
+++ lua/plugins/qol.lua
@@ -1,15 +1,25 @@
 return {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    ---@module 'snacks'
-    ---@type snacks.Config
-    opts = {
-        input = {
-            enabled = true, -- Enhances Ask
-        },
-        picker = {
-            enabled = true, -- Enhances Select
+    {
+        "folke/snacks.nvim",
+        priority = 1000,
+        lazy = false,
+        ---@module 'snacks'
+        ---@type snacks.Config
+        opts = {
+            input = {
+                enabled = true, -- Enhances Ask
+            },
+            picker = {
+                enabled = true, -- Enhances Select
+            },
         },
     },
+    {
+        "sphamba/smear-cursor.nvim",
+        opts = {},
+    },
+    {
+        "karb94/neoscroll.nvim",
+        opts = {},
+    }
 }
